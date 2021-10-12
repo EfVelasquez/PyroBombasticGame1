6f4496ae9d12510d2c3f4f7c02457ff15357d28e
EfVelasquez <efvb1@alu.ua.es>
2021-10-12 19:43:31 +0200

    fisicas

--- src/system/physics.s
+++ src/system/physics.s
@@ -24,11 +24,20 @@ phy_update_forone::
 
     add hl,bc
 
+    
+    ld a,h
+    cp #0
+    jp m, no_x
+    
+    add e_w(ix)
+    cp #81
+    jr nc, no_x
+
     ld e_x(ix), h
     ld e_x+1(ix), l
 
 
-
+    no_x:
     ld h,e_y(ix)
     ld l,e_y+1(ix)
 
@@ -37,60 +46,22 @@ phy_update_forone::
 
     add hl,bc
 
+    
+
+    ld a,h
+    cp #-2
+    jr nc, no_y
+    
+    add e_h(ix)
+    cp #201
+    jr nc, no_y
+
     ld e_y(ix), h
     ld e_y+1(ix), l
 
 
-    ;ld a, e_x(ix)
-    ;ld b, e_vx(ix) ;se carga en a la posicion en x
-    ;add a,b ;se suman, posicion en x + velocidad
-    ;
-    ;jp m, no_x
-;
-;
-    ;ld b, e_w(ix)
-    ;add a,b
-    ;
-    ;cp #81
-    ;
-;
-    ;jr nc, no_x
-    ;sub b
-;
-    ;ld e_x(ix), a ;se guarda en la posicion en x la nueva
-;
-    ;no_x:
-;
-;
-    ;;ld a, e_y(ix)
-    ;;ld b, e_vy(ix) ;se carga en a la posicion en x
-    ;;add a,b ;se suman, posicion en x + velocidad
-    ;;ld e_y(ix), a ;se guarda en la posicion en x la nueva
-;
-;
-    ;;;;;;;;;;;;;;;;;;
-    ;ld a, e_y(ix)
-    ;ld b, e_vy(ix) ;se carga en a la posicion en y
-    ;add a,b ;se suman, posicion en x + velocidad
-    ;
-    ;;jp m, end_rend ;;;;ESTA LINEA ME DETIENE A MITAD DE LA PANTALLA
-    ;inc a
-    ;jp z, end_rend
-    ;dec a
-;
-;
-    ;ld b, e_h(ix)
-    ;add a,b
-    ;
-    ;cp #201
-    ;
-;
-    ;jr nc, end_rend
-    ;sub b
-;
-    ;ld e_y(ix), a ;se guarda en la posicion en x la nueva
-;
-    ;end_rend:
-;
+    no_y:
+
+
 ret
 
