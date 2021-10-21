ea7b03efce5a6267c84267984cb82f4bc20ab295
EfVelasquez <efvb1@alu.ua.es>
2021-10-21 02:18:02 +0200

    arreglado desastre de food en ui

--- src/system/ui.h.s
+++ src/system/ui.h.s
@@ -1,6 +1,6 @@
 .globl sys_ui_init
 .globl sys_ui_add_round
 .globl sys_ui_erase_heart
-.globl sys_ui_init_food_life
-.globl sys_ui_food_decrease
+;.globl sys_ui_init_food_life
+;.globl sys_ui_food_decrease
 .globl sys_ui_food_damaged
--- src/system/ui.s
+++ src/system/ui.s
@@ -160,7 +160,9 @@ sys_ui_init::
     ;;ld   iy, #puntString   ;; IY = Pointer to the string 
     ;;call cpct_drawStringM0_asm  ;; Draw the string     
     
-    call sys_ui_init_food_life
+    ;call sys_ui_init_food_life
+    ld a, #16
+    ld (food_life), a
 
 ret
 
@@ -325,11 +327,11 @@ sys_ui_add_round::
 ret
 
 sys_ui_food_damaged::
-    ld a, (food_life)
-    dec a
+    ld hl, #food_life
+    dec (hl)
     call z, sys_ui_food_die
-    ld (food_life), a
-    call sys_ui_food_decrease
+    
+    ;call sys_ui_food_decrease
 ret
 
 
@@ -383,90 +385,90 @@ sys_ui_draw_food_counter::
 
     no_comida:
 ret
-
-
-sys_ui_init_food_life::
-
-    ld a, #16
-    ld (food_life), a
-
-    ld de, #0xC000
-    ld c, #31      ;; 61
-    ld b, #85       ;; 2
-    call cpct_getScreenPtr_asm
-    ex de, hl
-
-    ld hl, #food_life_pos
-
-    ld (hl), e
-    inc hl
-    ld (hl), d
-
-    ld hl, #food_life_pos
-    ;;ex de, hl
-    ld e, (hl)
-    inc hl
-    ld d, (hl)
-
-    ld c, #18
-    ld b, #6
-
-    ld a, #0xFF
-    call cpct_drawSolidBox_asm
-
-
-
-    ld de, #0xC000
-    ld c, #32       ;; 62
-    ld b, #86       ;; 3
-    call cpct_getScreenPtr_asm
-    ex de, hl
-
-    ld hl, #food_life_pos
-
-    ld (hl), e
-    inc hl
-    ld (hl), d
-
-    ld hl, #food_life_pos
-    ;;ex de, hl
-    ld e, (hl)
-    inc hl
-    ld d, (hl)
-
-    ld c, #16
-    ld b, #4
-
-    ld a, #0xF0
-    call cpct_drawSolidBox_asm
-
-    ld de, #0xC000
-    ld c, #48       ;; 78
-    ld b, #86        ;; 3
-    call cpct_getScreenPtr_asm
-    ex de, hl
-
-    ld hl, #food_life_pos
-
-    ld (hl), e
-    inc hl
-    ld (hl), d
-ret
-
-sys_ui_food_decrease::
-    ld hl, #food_life_pos
-    dec (hl)
-    ld e, (hl)
-    inc hl
-    ld d, (hl)
-    ld c, #1
-    ld b, #4
-    ld a, #0x30
-    call cpct_drawSolidBox_asm
-ret
+;
+;
+;sys_ui_init_food_life::
+;
+;    ld a, #16
+;    ld (food_life), a
+;
+;    ld de, #0xC000
+;    ld c, #31      ;; 61
+;    ld b, #85       ;; 2
+;    call cpct_getScreenPtr_asm
+;    ex de, hl
+;
+;    ld hl, #food_life_pos
+;
+;    ld (hl), e
+;    inc hl
+;    ld (hl), d
+;
+;    ld hl, #food_life_pos
+;    ;;ex de, hl
+;    ld e, (hl)
+;    inc hl
+;    ld d, (hl)
+;
+;    ld c, #18
+;    ld b, #6
+;
+;    ld a, #0xFF
+;    call cpct_drawSolidBox_asm
+;
+;
+;
+;    ld de, #0xC000
+;    ld c, #32       ;; 62
+;    ld b, #86       ;; 3
+;    call cpct_getScreenPtr_asm
+;    ex de, hl
+;
+;    ld hl, #food_life_pos
+;
+;    ld (hl), e
+;    inc hl
+;    ld (hl), d
+;
+;    ld hl, #food_life_pos
+;    ;;ex de, hl
+;    ld e, (hl)
+;    inc hl
+;    ld d, (hl)
+;
+;    ld c, #16
+;    ld b, #4
+;
+;    ld a, #0xF0
+;    call cpct_drawSolidBox_asm
+;
+;    ld de, #0xC000
+;    ld c, #48       ;; 78
+;    ld b, #86        ;; 3
+;    call cpct_getScreenPtr_asm
+;    ex de, hl
+;
+;    ld hl, #food_life_pos
+;
+;    ld (hl), e
+;    inc hl
+;    ld (hl), d
+;ret
+;
+;sys_ui_food_decrease::
+;    ld hl, #food_life_pos
+;    dec (hl)
+;    ld e, (hl)
+;    inc hl
+;    ld d, (hl)
+;    ld c, #1
+;    ld b, #4
+;    ld a, #0x30
+;    call cpct_drawSolidBox_asm
+;ret
 
 ;;AQUI LA VIDA DE LA COMIDA ES 0
 sys_ui_food_die::
-    call sys_ui_food_decrease
+    ;call sys_ui_food_decrease
     call screen_man_death_screen
 ret
